ae0332c0f9baa6695e0fab1401e448fe50cbd27a
Thomas Quintana <thomas@ubuntu.(none)>
2013-12-09 13:26:09 -0500

    First Commit.

---
+++ fsm.py
@@ -0,0 +1,219 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Thomas Quintana <quintana.thomas@gmail.com>
+import types
+
+class FiniteStateMachineError(Exception):
+  def __init__(self, message):
+    self.message = message
+
+  def __str__(self):
+    return self.message
+
+class Action(object):
+  def __init__(self, *args, **kwargs):
+    self.args = args
+    self.kwargs = kwargs
+
+  def __call__(self, function):
+    # Append metadata to the function that is necessary
+    # to build a transition lookup table by the finite
+    # state machine.
+    state = self.kwargs.get('state')
+    if not state \
+      or not type(state) == types.StringType \
+      or len(state) == 0:
+      raise FiniteStateMachineError('Please specify a valid action \
+       state attribute.\n Possible values are strings.')
+    else:
+      function.__fsm_action_state__ = state
+    if self.kwargs.has_key('on_enter'):
+      on_enter = self.kwargs.get('on_enter')
+      if type(on_enter) == types.BooleanType:
+        function.__fsm_action_on_enter__ = on_enter
+      else:
+        raise TypeError('Please specify a valid action on_enter \
+          attribute.\n Possible values are True or False.')
+    else:
+      function.__fsm_action_on_enter__ = True
+    if self.kwargs.has_key('on_exit'):
+      on_exit = self.kwargs.get('on_exit')
+      if type(on_exit) == types.BooleanType:
+        function.__fsm_action_on_exit__ = on_exit
+      else:
+        raise TypeError('Please specify a valid action on_exit \
+          attribute.\n Possible values are True or False.')
+    else:
+      function.__fsm_action_on_exit__ = False
+    function.__fsm_action__ = True
+    return function
+
+class Guard(object):
+  def __init__(self, *args, **kwargs):
+    self.args = args
+    self.kwargs = kwargs
+
+  def __call__(self, function):
+    # Append metadata to the function that is necessary
+    # to build a transition lookup table by the finite
+    # state machine.
+    state = self.kwargs.get('state')
+    if not state \
+      or not type(state) == types.StringType \
+      or len(state) == 0:
+      raise TypeError('Please specify a valid guard state attribute.\n\
+        Possible values are strings.')
+    else:
+      function.__fsm_guard_state__ = state
+    function.__fsm_guard__ = True
+    return function
+
+class FiniteStateMachine(object):
+  def __init__(self):
+    self.__fsm_transition_table__ = self.__create_lookup_table__()
+
+  def __create_lookup_table__(self):
+    actions = self.__get_actions__()
+    guards = self.__get_guards__()
+    states = self.__get_states__()
+    state_map = self.__create_state_map__(actions, guards, states)
+    # Create the lookup table.
+    lookup_table = dict()
+    for begin, end in self.transitions:
+      transitions = lookup_table.get(begin)
+      if not transitions:
+        transitions = dict()
+        lookup_table.update({ begin: transitions })
+      transition = transitions.get(end)
+      if not transition:
+        transition = dict()
+        transitions.update({ end: transition })
+      if not transition.has_key('beginning_state'):
+        transition.update({ 'beginning_state': state_map.get(begin) })
+      if not transition.has_key('end_state'):
+        transition.update({ 'end_state': state_map.get(end) })
+    return lookup_table
+
+  def __create_state_map__(self, actions, guards, states):
+    state_map = dict()
+    # Make sure every state has an entry.
+    for state in states:
+      state_map.update({ state: dict() })
+    # Attach actions to their states.
+    for action in actions:
+      state_name = action.__fsm_action_state__
+      if state_name not in states:
+        raise FiniteStateMachineError('A state named %s is not declared \
+          in the transitions list.\n Please add %s to the list of possible \
+          transitions or modify the @Action decorator on %s.' % (state_name, 
+          state_name, action.__name__))
+      state = state_map.get(state_name)
+      on_enter = action.__fsm_action_on_enter__
+      if not state.has_key('on_enter'):
+        if on_enter:
+          state.update({ 'on_enter': action })
+      else:
+        raise FiniteStateMachineError('The %s state can only have one \
+          action declared for on_enter.' % (state_name))
+      on_exit = action.__fsm_action_on_exit__
+      if not state.has_key('on_exit'):
+        if on_exit:
+          state.update({ 'on_exit': action })
+      else:
+        raise FiniteStateMachineError('The %s state can only have one \
+          action declared for on_exit.' % (state_name))
+    # Attach guards to their states.
+    for guard in guards:
+      state_name = guard.__fsm_guard_state__
+      if state_name not in states:
+        raise FiniteStateMachineError('A state named %s is not declared \
+          in the transitions list.\n Please add %s to the list of possible \
+          transitions or modify the @Guard decorator on %s.' % (state_name,
+          state_name, guard.__name__))
+      state = state_map.get(state_name)
+      if not state.has_key('guard'):
+        state.update({ 'guard': guard })
+      else:
+        raise FiniteStateMachineError('The %s state can only have one guard \
+          declared' % (state_name))
+    return state_map
+
+  def __get_actions__(self):
+    return filter(
+      lambda callable: hasattr(callable, '__fsm_action__'),
+      self.__get_callables__()
+    )
+
+  def __get_callables__(self):
+    actions = list()
+    results = dir(self)
+    for result in results:
+      attr = getattr(self, result)
+      if hasattr(attr, '__call__'):
+        actions.append(attr)
+    return actions
+
+  def __get_guards__(self):
+    return filter(
+      lambda callable: hasattr(callable, '__fsm_guard__'),
+      self.__get_callables__()
+    )
+
+  def __get_states__(self):
+    # Make sure the user declared a set of possible transitions.
+    if not self.transitions:
+      raise FiniteStateMachineError('Please specify a list of possible \
+        transitions.\n Each entry in the list is a two-tuple where the \
+        first value is the beginning state and the second value is the \
+        end state.')
+    states = list()
+    for begin, end in self.transitions:
+      if begin not in states:
+        states.append(begin)
+      if end not in states:
+        states.append(end)
+    return states
+
+  def transition(self, to = None, event = None):
+    # Make sure we are in a good state.
+    transitions = self.__fsm_transition_table__.get(self.state)
+    if not transitions:
+      raise FiniteStateMachineError('The %s state is invalid, or we \
+        have entered a terminal state.' % (self.state))
+    # Try to find the desired transition.
+    transition = transitions.get(to)
+    if not transition:
+      raise FiniteStateMachineError('The transition from %s to %s is \
+        invalid.' % (self.state, to))
+    # If there are any guards lets execute those now.
+    if transition.get('end_state').has_key('guard'):
+      allowed = transition.get('end_state').get('guard')()
+      if not type(allowed) == types.BooleanType:
+        raise FiniteStateMachineError('A guard must only return True \
+          or False values.')
+      if not allowed:
+        raise FiniteStateMachineError('A guard declined the transition \
+          from %s to %s.' % (self.state, to))
+    # Try to execute the action associated with leaving the current state.
+    if transition.get('beginning_state').has_key('on_exit'):
+      transition.get('beginning_state').get('on_exit')(event)
+    # Try to execute the action associated with entering the new state.
+    if transition.get('end_state').has_key('on_enter'):
+      transition.get('end_state').get('on_enter')(event)
+    # Enter the new state and we're done.
+    self.state = to
---
+++ tests.py
@@ -0,0 +1,189 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Thomas Quintana <quintana.thomas@gmail.com>
+from fsm import *
+from unittest import TestCase, expectedFailure
+
+class AbstractLightBulb(FiniteStateMachine):
+  # Current state.
+  state = 'off'
+
+  # Possible transitions.
+  transitions = [
+    ('off', 'on'),
+    ('on', 'off'),
+    ('off', 'broken'),
+    ('on', 'broken')
+  ]
+
+class LightBulb(AbstractLightBulb):
+  # Handle incoming events.
+  def on_message(self, message):
+    if message == 'turn on':
+      self.transition(to = 'on', event = message)
+    elif message == 'turn off':
+      self.transition(to = 'off', event = message)
+    elif message == 'break':
+      self.transition(to = 'broken', event = message)
+
+  @Guard(state = 'on')
+  def check_electricity(self):
+    if hasattr(self, 'electricity'):
+      return self.electricity
+    return False
+
+  @Action(state = 'off')
+  def turn_off(self, message):
+    self.indicator = 'dim'
+
+  @Action(state = 'on')
+  def turn_on(self, message):
+    self.indicator = 'lit'
+
+  @Action(state = 'broken')
+  def smash(self, message):
+    self.indicator = 'broken'
+
+class LightBulbWithMultipleGuards(AbstractLightBulb):
+  @Guard(state = 'on')
+  def check_electricity(self):
+    pass
+
+  @Guard(state = 'on')
+  def check_socket(self):
+    pass
+
+class LightBulbWithMultipleOnEnterActions(AbstractLightBulb):
+  @Action(state = 'on')
+  def turn_on(self, message):
+    pass
+
+  @Action(state = 'on')
+  def turn_on_again(self, message):
+    pass
+
+class LightBulbWithMultipleOnExitActions(AbstractLightBulb):
+  @Action(state = 'on', on_enter = False, on_exit = True)
+  def turn_on(self, message):
+    pass
+
+  @Action(state = 'on', on_enter = False, on_exit = True)
+  def turn_on_again(self, message):
+    pass
+
+class LightBulbWithBadActionState(AbstractLightBulb):
+  @Action(state = 'bogus')
+  def foobar(self, message):
+    pass
+
+class LightBulbWithBadGuardState(AbstractLightBulb):
+  @Guard(state = 'bogus')
+  def foobar(self):
+    pass
+
+class LightBulbTests(TestCase):
+  def test_success_scenario(self):
+    light_bulb = LightBulb()
+    light_bulb.electricity = True
+    self.assertTrue(light_bulb.state == 'off')
+    light_bulb.on_message('turn on')
+    self.assertTrue(light_bulb.state == 'on')
+    self.assertTrue(light_bulb.indicator == 'lit')
+    light_bulb.on_message('turn off')
+    self.assertTrue(light_bulb.state == 'off')
+    self.assertTrue(light_bulb.indicator == 'dim')
+
+  def test_invalid_transition(self):
+    light_bulb = LightBulb()
+    light_bulb.electricity = True
+    self.assertTrue(light_bulb.state == 'off')
+    try:
+      light_bulb.on_message('turn off')
+    except FiniteStateMachineError as error:
+      pass
+    self.assertFalse(hasattr(light_bulb, 'indicator'))
+
+  def test_terminal_state(self):
+    light_bulb = LightBulb()
+    light_bulb.electricity = True
+    self.assertTrue(light_bulb.state == 'off')
+    light_bulb.on_message('turn on')
+    self.assertTrue(light_bulb.state == 'on')
+    self.assertTrue(light_bulb.indicator == 'lit')
+    light_bulb.on_message('turn off')
+    self.assertTrue(light_bulb.state == 'off')
+    self.assertTrue(light_bulb.indicator == 'dim')
+    light_bulb.on_message('break')
+    self.assertTrue(light_bulb.state == 'broken')
+    self.assertTrue(light_bulb.indicator == 'broken')
+    try:
+      light_bulb.on_message('turn on')
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb.state == 'broken')
+    self.assertTrue(light_bulb.indicator == 'broken')
+
+  def test_guard_denial(self):
+    light_bulb = LightBulb()
+    self.assertTrue(light_bulb.state == 'off')
+    try:
+      light_bulb.on_message('turn on')
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb.state == 'off')
+    self.assertFalse(hasattr(light_bulb, 'indicator'))
+
+  def test_multiple_guards(self):
+    light_bulb = None
+    try:
+      light_bulb = LightBulbWithMultipleGuards()
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb == None)
+
+  def test_multiple_on_enter_actions(self):
+    light_bulb = None
+    try:
+      light_bulb = LightBulbWithMultipleOnEnterActions()
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb == None)
+
+  def test_multiple_on_exit_actions(self):
+    light_bulb = None
+    try:
+      light_bulb = LightBulbWithMultipleOnExitActions()
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb == None)
+
+  def test_bad_action_state(self):
+    light_bulb = None
+    try:
+      light_bulb = LightBulbWithBadActionState()
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb == None)
+
+  def test_bad_guard_state(self):
+    light_bulb = None
+    try:
+      light_bulb = LightBulbWithBadGuardState()
+    except FiniteStateMachineError as error:
+      pass
+    self.assertTrue(light_bulb == None)
