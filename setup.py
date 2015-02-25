68f6a6373239c8352bc8979fcffcba95c7b8af37
iuryAlves <satriani-16@hotmail.com>
2015-02-24 21:43:51 -0300

    criando setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="declarative-fsm",
+    version="",
+    py_modules=["fsm"],
+    author="Thomas Quintana",
+    author_email="quintana.thomas@gmail.com",
+    description='',
+    license="PROPRIETARY",
+    url="",
+)
